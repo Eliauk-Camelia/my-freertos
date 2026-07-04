4b888937c6981ca1b5b7d1f167e7acf483219087
Eliauk_Camelia <3796336114@qq.com>
2026-07-04 20:28:45 +0800

    feat(rtos): add linux-style generic list and task_struct header

--- rtos.h
+++ rtos.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-
 struct list_head{
     struct list_head *next;
     struct list_head *prev;
@@ -18,6 +17,39 @@ static inline void INIT_LIST_HEAD(struct list_head *head)
     head->prev = head;
 }
 
+static inline void __list_add(struct list_head *new_node, struct list_head *prev, struct list_head *next)
+{
+    next->prev = new_node;
+    new_node->next = next;
+    new_node->prev = prev;
+    prev->next = new_node;
+}
 
+static inline void list_add_tail(struct list_head *new_node,struct list_head *head)
+{
+    __list_add(new_node,head->prev,head);
+}
+
+static inline void list_del(struct list_head *entry)
+{
+    entry->next->prev = entry->prev;
+    entry->prev->next = entry->next;
+    entry->next = nullptr;
+    entry->prev = nullptr;
+}
+
+/* 核心宏：通过链表节点反向拿到宿主结构体（对应FreeRTOS偏移计算） */
+#define container_of(ptr, type, member) ({			\
+	const typeof(((type *)0)->member) *__mptr = (ptr);	\
+	(type *)((char *)__mptr - offsetof(type, member));})
+
+#define list_for_each(pos,head) for(pos = (head)->next ;pos != head;pos = pos->next)
+
+/*任务结构体*/
+struct task_struct{
+    uint32_t *stack_top;
+    void (*task_func)(void);
+    struct list_head list;
+};
 
 #endif
