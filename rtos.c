9b6dbe0d1a9b62a5a5286e14f77cdc40bbacce1a
Eliauk_Camelia <3796336114@qq.com>
2026-07-04 21:49:16 +0800

    feat(rtos): implement task_create to allocate and link task

--- rtos.c
+++ rtos.c
@@ -12,3 +12,23 @@ void rtos_init(void)
     task_count = 0;
     INIT_LIST_HEAD(&ready_task_head);
 }
+
+/*创建任务*/
+int task_create(uint32_t *stack_top,void (*func)(void))
+{
+    if(task_count >= MAX_TASK_NUM)
+    {
+        return -1;
+    }
+
+    struct task_struct *tsk = &task_pool[task_count];
+    tsk->stack_top = stack_top;
+    tsk->task_func =func;
+    INIT_LIST_HEAD(&tsk->list);
+
+    list_add_tail(&tsk->list,&ready_task_head);
+    task_count ++;
+
+    return 0;
+}
+
